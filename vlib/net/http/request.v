7debed9b6f5f5d12e697e3a5b8134fcbdb38152c
Jef Roosens <roosensjef@gmail.com>
2022-01-09 18:22:44 +0100

    Added parse_request_no_body

--- vlib/net/http/request.v
+++ vlib/net/http/request.v
@@ -172,6 +172,29 @@ pub fn (req &Request) referer() string {
 	return req.header.get(.referer) or { '' }
 }
 
+pub fn parse_request_no_body(mut reader io.BufferedReader) ?Request {
+	// request line
+	mut line := reader.read_line() ?
+	method, target, version := parse_request_line(line) ?
+
+	// headers
+	mut header := new_header()
+	line = reader.read_line() ?
+	for line != '' {
+		key, value := parse_header(line) ?
+		header.add_custom(key, value) ?
+		line = reader.read_line() ?
+	}
+	header.coerce(canonicalize: true)
+
+	return Request{
+		method: method
+		url: target.str()
+		header: header
+		version: version
+	}
+}
+
 // Parse a raw HTTP request into a Request object
 pub fn parse_request(mut reader io.BufferedReader) ?Request {
 	// request line
